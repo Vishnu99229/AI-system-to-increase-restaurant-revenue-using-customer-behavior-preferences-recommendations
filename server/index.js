53dce87ad21558bd63d6a16c696993197747a34b
“Vishnu” <vishnu.rajan294@gmail.com>
2026-04-24 15:25:16 +0530

    add admin.orlena.talk to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -192,6 +192,7 @@ const authenticateAdmin = async (req, res, next) => {
 const ALLOWED_ORIGINS = [
     "https://orlena.talk",
     "https://app.orlena.talk",
+    "https://admin.orlena.talk",
     "https://api.orlena.talk",
     "https://qr-menu-upsell.vercel.app",
     "http://localhost:5173",
